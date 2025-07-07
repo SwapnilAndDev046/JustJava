38bc98934e8f248015016bfddae44ecb0711ba08
SwapnilAndDev046 <swapnildevkategmi@gmail.com>
2025-07-07 17:46:58 +0530

    Shadowing is best

--- Day6ReviseThisEveryDay/IMPORTANT/Shadowing.java
+++ Day6ReviseThisEveryDay/IMPORTANT/Shadowing.java
@@ -2,8 +2,15 @@ package Day6ReviseThisEveryDay.IMPORTANT;
 
 public class Shadowing {
   static int a = 4;// what ever we are writing in the class should contain static
+  
   public static void main(String[] args) {
-   System.out.println(a); 
+    System.out.println(a);//4
+   int a = 90;
+   System.out.println(a); //90
+   fun();
+  }
+  static void fun(){
+System.out.println(a);//4
   }
 
 }
