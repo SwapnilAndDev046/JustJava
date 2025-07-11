36ed92deb85213b5a95749d8a6c3e0195dab7b53
SwapnilAndDev046 <swapnildevkategmi@gmail.com>
2025-07-11 12:18:30 +0530

    f

--- Day7/Strings/Pooling.java
+++ Day7/Strings/Pooling.java
@@ -4,6 +4,7 @@ public class Pooling {
   static void pooling(){
     String a = "Hello";
     String b = "Hello";
+    System.out.println(a==b);
   }
   public static void main(String[] args) {
     pooling();
