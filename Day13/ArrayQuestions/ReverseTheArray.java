e57d4ab6d07d700934d684120c9de1e996347564
Swapnil Devkate <140427650+SwapnilAndDev046@users.noreply.github.com>
2025-07-28 15:56:55 +0530

    Reverse array

--- Day13/ArrayQuestions/ReverseTheArray.java
+++ Day13/ArrayQuestions/ReverseTheArray.java
@@ -1,15 +1,13 @@
 package Day13.ArrayQuestions;
 
-import java.util.ArrayList;
+// import java.util.ArrayList;
 import java.util.Arrays;
 
 public class ReverseTheArray {
     public static void main(String[] args) {
         int [] arr ={1,2,3,4,5,6,7,8,9};
-        // reverse(arr);
-        reverse2(arr);
-        // System.out.println(Arrays.toString(arr));
-      
+        reverse2(arr);// Empty String
+        reverse(arr);// two pointer (converging)
     }
     static void reverse(int [] arr){
         int start = 0;
@@ -21,13 +19,15 @@ public class ReverseTheArray {
             start++;
             end--;
         }
+        System.out.println("Using Converging two pointers: "+Arrays.toString(arr));
     }
     static void reverse2(int []arr){
         int  n = arr.length-1;
-        ArrayList <Integer> list = new ArrayList<>();
-         for (int i = n; i < 0; i--) {
-            list.add(arr[i]);
+        String s = "";
+         for (int i = n; i >= 0; i--) {
+           s = s +" "+ arr[i];
         }
-        System.out.println(list);
+        System.out.println("Using Empty String: "+s);
+      
     }
 }
