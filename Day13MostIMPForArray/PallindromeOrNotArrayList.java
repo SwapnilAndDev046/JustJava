d703dc5a4929da19506fe8a5aacc00101c86ac0c
Swapnil Devkate <140427650+SwapnilAndDev046@users.noreply.github.com>
2025-07-29 15:21:21 +0530

    Day13 Got Over

--- Day13MostIMPForArray/PallindromeOrNotArrayList.java
+++ Day13MostIMPForArray/PallindromeOrNotArrayList.java
@@ -5,25 +5,27 @@ import java.util.Scanner;
 
 public class PallindromeOrNotArrayList {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        ArrayList <Integer> list = new ArrayList<>();
-        for (int i = 0; i < 5; i++) {
-            list.add(in.nextInt());
-        }
-        if (isPallindrome(list)) {
-            System.out.println("It's pallindrome");
-        }else{
-            System.out.println("It's not a pallindrome");
+        try (Scanner in = new Scanner(System.in)) {
+            ArrayList<Integer> list = new ArrayList<>();
+            for (int i = 0; i < 5; i++) {
+                list.add(in.nextInt());
+            }
+            if (isPallindrome(list)) {
+                System.out.println("It's pallindrome");
+            } else {
+                System.out.println("It's not a pallindrome");
+            }
         }
     }
-    static boolean isPallindrome(ArrayList <Integer> list){
+
+    static boolean isPallindrome(ArrayList<Integer> list) {
         int start = 0;
-        int end  = list.size()-1;
-        while (start<end) {
-            if (list.get(end)==list.get(start)) {
+        int end = list.size() - 1;
+        while (start < end) {
+            if (list.get(end) == list.get(start)) {
                 start++;
                 end--;
-            }else{
+            } else {
                 return false;
             }
         }
