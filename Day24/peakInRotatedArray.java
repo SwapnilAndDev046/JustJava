f078a1c736429af16ed939dbee3c252aee8e5b77
Swapnil Devkate <140427650+SwapnilAndDev046@users.noreply.github.com>
2025-09-06 17:23:47 +0530

    peakinRotated Array

--- Day24/peakInRotatedArray.java
+++ Day24/peakInRotatedArray.java
@@ -1,8 +1,30 @@
 package Day24;
 
 public class peakInRotatedArray {
-    
+    static int peak(int[] arr) {
+        int n = arr.length;
+        int low = 0;
+        int high = n - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (high > mid && arr[mid] > arr[mid + 1]) {
+                return mid;
+            }
+            if (mid > low && arr[mid] < arr[mid - 1]) {
+                return mid - 1;
+            }
+            if (arr[low] <= arr[mid]) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+
     public static void main(String[] args) {
-        
+        int[] arr = { 8, 9, 0, 1, 2, 3, 4, 5, 6, 7 };
+
+        System.out.println(peak(arr));
     }
 }
